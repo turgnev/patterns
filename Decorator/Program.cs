c8336a6d6795858b05f93f8e831ca905c96f5671
vlad <vladislav_turgen@mail.ru>
2020-01-06 01:07:22 +0300

    Decorator added

--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -2,11 +2,55 @@
 
 namespace Decorator
 {
+    abstract class Coffee
+    {
+        public string Description;
+
+        public decimal Price;
+    }
+
+    class Cappuccinno : Coffee
+    {
+        public Cappuccinno()
+        {
+            Description = nameof(Cappuccinno);
+            Price = 5;
+        }
+    }
+
+    class MilkDecorator : Coffee
+    {
+        Coffee baseBeverage;
+
+        public MilkDecorator(Coffee baseBeverage)
+        {
+            this.baseBeverage = baseBeverage;
+            Description = baseBeverage.Description + " + Milk";
+            Price = baseBeverage.Price + 3;
+        }     
+    }
+
+    class SoyDecorator : Coffee
+    {
+        Coffee baseBeverage;
+
+        public SoyDecorator(Coffee baseBeverage)
+        {
+            this.baseBeverage = baseBeverage;
+            Description = baseBeverage.Description + " + Soy";
+            Price = baseBeverage.Price + 2;
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var coffee = new SoyDecorator(new MilkDecorator(new Cappuccinno()));
+
+            Console.WriteLine($"{coffee.Description} - {coffee.Price}");
+            Console.ReadLine();
         }
     }
 }
