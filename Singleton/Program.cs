737bda8683a654a2ecf33380c8cd2129a129ccbf
vlad <vladislav_turgen@mail.ru>
2020-01-06 23:52:26 +0300

    Singleton added

---
+++ Singleton/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Singleton
+{
+    class SingletonObject
+    {
+        private SingletonObject()
+        {
+        }
+
+        private static SingletonObject uniqueObject;
+
+        public static SingletonObject GetInstance()
+        {
+            if (uniqueObject == null)
+                uniqueObject = new SingletonObject();
+
+            return uniqueObject;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingletonObject a = SingletonObject.GetInstance();
+            SingletonObject b = SingletonObject.GetInstance();
+
+            Console.ReadKey();
+        }
+    }
+}
