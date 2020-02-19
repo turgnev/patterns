754b73ed445c7c0f6ef836386e6e47e8e676fe9a
vlad <vladislav_turgen@mail.ru>
2020-02-20 00:17:30 +0300

    FactoryMethod Added

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+
+    abstract class Door
+    {
+        public string Material;
+
+    }
+
+    class WoodenDoor : Door
+    {
+        public WoodenDoor()
+        {
+            this.Material = "wood";
+        }
+    }
+
+    class SteelDoor : Door
+    {
+        public SteelDoor()
+        {
+            this.Material = "steel";
+        }
+    }
+
+    abstract class DoorMaker
+    {
+        public abstract Door Create();
+
+    }
+
+    class WDoorMaker : DoorMaker
+    {
+        public override Door Create()
+        {
+            return new WoodenDoor();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoorMaker maker = new WDoorMaker();
+
+            Door a = maker.Create();
+            Console.Write(a.Material);
+
+            Console.Read();
+        }
+    }
+}
